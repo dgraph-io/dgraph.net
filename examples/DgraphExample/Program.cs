f48611c217f0eb071992518dda1dd808d79c33fb
Michel Diz <MichelDiz@users.noreply.github.com>
2023-04-17 16:16:21 -0300

    Upgrade the lib to .NET 7 (#28)
    * upgrade the lib to .NET 7
    
    * Start the basis of the example
    
    * Fix EOF

---
+++ examples/DgraphExample/Program.cs
@@ -0,0 +1,27 @@
+﻿using Dgraph;
+
+namespace DgraphExample
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // Connect to Dgraph Cloud.
+            var cloudUrl = "https://green-bird.grpc.us-east-1.aws.cloud.dgraph.io/graphql";
+            var APIKEY = "xxx=";
+            var client = new DgraphClient(SlashChannel.Create(cloudUrl, APIKEY));
+
+            var version = await client.CheckVersion();
+
+            Console.WriteLine("Version: " + version.Value);
+
+            // Perform a query.
+            string query = @"schema{}";
+
+            var readOnlyTransaction = client.NewTransaction();
+            var response = await readOnlyTransaction.Query(query);
+            Console.WriteLine("Query response: " + response.Value.Json);
+
+        }
+    }
+}
