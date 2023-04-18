23a70b556a58deede495cf7e4ffb9b756ff87559
micheldiz <michelconrado@outlook.com>
2023-04-17 20:19:26 -0700

    update example

---
+++ examples/DgraphExample/ExecuteDQLQuery.cs
@@ -0,0 +1,65 @@
+using Dgraph;
+using Dgraph.Transactions;
+
+namespace DgraphExample
+{
+    class ExecuteDQL
+    {
+        public static async Task<string> Query(DgraphClient Client, string query)
+        {
+            using (ITransaction transaction = Client.NewTransaction())
+            {
+                try
+                {
+                    // Perform a query.
+                    var response = await transaction.Query(query);
+                    await transaction.Commit();
+
+                    if (response.IsFailed)
+                    {
+                        Console.WriteLine($"[{DateTime.Now}] gRPC response failed: {response.Errors[0].Message}");
+                        return "gRPC Got error";
+                    }
+                    else
+                    {
+                        return response.Value.Json;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"[{DateTime.Now}] An error occurred during the query: {ex.Message}");
+                    await transaction.Discard();
+                    return "gRPC Error during transaction";
+                }
+            }
+        }
+                public static async Task<string> QueryWithRetry(DgraphClient Client, string query)
+        {
+            using (ITransaction transaction = Client.NewRetryableTransaction())
+            {
+                try
+                {
+                    // Perform a query.
+                    var response = await transaction.Query(query);
+                    await transaction.Commit();
+
+                    if (response.IsFailed)
+                    {
+                        Console.WriteLine($"[{DateTime.Now}] gRPC response failed: {response.Errors[0].Message}");
+                        return "gRPC Got error";
+                    }
+                    else
+                    {
+                        return response.Value.Json;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"[{DateTime.Now}] An error occurred during the query: {ex.Message}");
+                    await transaction.Discard();
+                    return "gRPC Error during transaction";
+                }
+            }
+        }
+    }
+}
--- examples/DgraphExample/Program.cs
+++ examples/DgraphExample/Program.cs
@@ -8,7 +8,7 @@ namespace DgraphExample
         {
             // Connect to Dgraph Cloud.
             var cloudUrl = "https://green-bird.grpc.us-east-1.aws.cloud.dgraph.io/graphql";
-            var APIKEY = "MjM2MjA5YTYzMmViODJjYTA2NWM4YmVhY2EzZDgzNTE=";
+            var APIKEY = "=";
             using var client = new DgraphClient(SlashChannel.Create(cloudUrl, APIKEY));
 
             var version = await client.CheckVersion();
@@ -18,23 +18,14 @@ namespace DgraphExample
                 Console.WriteLine("Version: " + version.Value);
             }
 
-            // Perform a query.
             string query = @"schema{}";
 
-            // Cria uma transação retryable
-            using var txn = client.NewRetryableTransaction();
+            string result = await ExecuteDQL.Query(client, query);
 
-            var response = await txn.Query(query);
-
-            if (response.IsSuccess)
-            {
-                Console.WriteLine("Query response: " + response.Value.Json);
-            }
-            else
-            {
-                Console.WriteLine("Error: " + response.Errors.First().Message);
-            }
+            // Print the result
+            Console.WriteLine(result);
 
         }
+
     }
 }
